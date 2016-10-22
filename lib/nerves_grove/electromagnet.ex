043de3aa9332063c22c8f46e5e334f390996a3dc
Guillaume Ballet <gballet@gmail.com>
2016-10-22 17:54:13 +0200

    Add support for the electromagnet

---
+++ lib/nerves_grove/electromagnet.ex
@@ -0,0 +1,75 @@
+# This is free and unencumbered software released into the public domain.
+
+defmodule Nerves.Grove.EM do
+  @moduledoc """
+  Seeed Studio Grove Electromagnet
+
+  # Example
+
+      alias Nerves.Grove.EM
+
+      {:ok, desc} = EM.start_link(pin)
+
+      EM.on(desc)
+
+  # Configuration
+
+  EMs can be driven either directly by GPIOs or through and i2c bus.
+  Their configuration is specified as follows:
+
+      config :nerves_grove, ..., pins: [
+                                        {2, :gpio, {}},
+                                        {4, :i2c, {"i2c-1", 0x04}} ]
+
+  In the case of GPIOs, only the pin number along with the atom `:gpio` and
+  an empty tuple is necessary. In the case of i2c, the pin number, `:i2c`, and
+  a tuple containing the file name and i2c address of the controller chip are
+  necessary.
+  """
+
+  @i2c_digital_read_command  1
+  @i2c_digital_write_command 2
+  @i2c_mode_command  5
+
+  @spec start_link(pos_integer) :: {:ok, pid} | {:error, any}
+  def start_link(pin) when is_integer(pin) do
+    # Look for the pin descriptor in the documentation. Assume GPIO otherwise.
+    pins = Application.get_env(:nerves_grove, :pins, [])
+    { _, type, extra } = Enum.find(pins, fn {pn,_,_} -> pn == pin end) || {pin, :gpio, nil}
+
+    # Depending on the "line" type, proceed to set the pin as an output GPIO.
+    case type do
+      :gpio ->
+        {success, pid} = Gpio.start_link(pin, :output)
+        {success, {pid, :gpio, pin}}
+      :i2c ->
+        { fname, address } = extra
+        with {:ok, pid} <- I2c.start_link(fname, address),
+             :ok <- I2c.write(pid, <<@i2c_mode_command, pin, 1, 0>>),
+           do: {:ok, {pid, :i2c, pin}}
+      _ -> {:error, "Invalid type #{type}"}
+    end    
+  end
+
+  @doc "Switches on the Electromagnet."
+  @spec on({pid,Keyword.T,pos_integer}) :: any
+  def on(data) when is_tuple(data) do
+    {pid, type, pin} = data
+    case type do
+      :gpio -> Gpio.write(pid, 1)
+      :i2c  -> I2c.write(pid, <<@i2c_digital_write_command, pin, 1, 0>>)
+      _     -> {:error, "Invalid line type"}
+    end
+  end
+
+  @doc "Switches off the Electromagnet."
+  @spec off({pid, Keyword.T, pos_integer}) :: any
+  def off(data) when is_tuple(data) do
+    {pid, type, pin} = data
+    case type do
+      :gpio -> Gpio.write(pin, 1)
+      :i2c  -> I2c.write(pid, <<@i2c_digital_write_command, pin, 0, 0>>)
+      _     -> {:error, "Invalid line type"}
+    end
+  end
+end
