afd936d19d1ab007bccc5589fe5f01d33e8f7e50
Guillaume Ballet <gballet@gmail.com>
2016-11-19 17:47:16 +0100

    Allow for reading the NFC EEPROM.

--- lib/nerves_grove/nfc.ex
+++ lib/nerves_grove/nfc.ex
@@ -1,5 +1,29 @@
 # This is free and unencumbered software released into the public domain.
 
 defmodule Nerves.Grove.NFC do
-  # TODO
+  @moduledoc """
+  Seeed Studio Grove NFC reader
+
+  # Example
+
+      alias Nerves.Grove.NFC
+
+      {:ok, pid} = NFC.start_link(address)
+
+      NFC.read_eeprom(size)
+  """
+
+  @default_nfc_address 0x53
+
+
+  @spec start_link(pos_integer) :: {:ok, pid} | :error
+  def start_link(address = @default_nfc_address) do
+    I2c.start_link("i2c-1", address)
+  end
+
+  @doc "Read the content of the EEPROM"
+  @spec read_eeprom(pid, 0..512) :: any
+  def read_eeprom(pid, size) do
+    I2c.read(pid, size)
+  end
 end
